20ce809ea74cb5edbc47f058ad7e7ebef35c2518
keerthikolisetty <kolisettykeerthi23@gmail.com>
2025-11-12 22:33:59 +0530

    Add files via upload

--- binary search.c
+++ binary search.c
@@ -1,28 +1,43 @@
-#include<stdio.h>
-int main()
-{
-	int i,n,key,pos=0,mid=0,low=0;
-printf("enter n value");
-scanf("%d",&n);
-int a[n];
-int high=n-1;
-for(i=0;i<n;i++)
-{
-	scanf("%d",&a[i]);
-}
-printf("enter key");
-scanf("%d",&key);
-while(low<=high)
-{
-	mid=(low+high)/2;
-	if (key==a[mid])
-	{
-		pos=mid;
-		break;
-	}
-	else{
-		low=mid+1;
-	}
-}
-	printf("element found at positin %d",pos);
+#include <stdio.h>
+
+int main() {
+    int n, key, low, high, mid, i, found = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements in sorted order:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+
+    low = 0;
+    high = n - 1;
+
+    while(low <= high) {
+        mid = (low + high) / 2;
+
+        if(arr[mid] == key) {
+            printf("Element %d found at position %d\n", key, mid + 1);
+            found = 1;
+            break;
+        }
+        else if(arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if(!found) {
+        printf("Element %d not found in the array.\n", key);
+    }
+
+    return 0;
 }
